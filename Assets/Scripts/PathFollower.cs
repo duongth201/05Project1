64138339fc46e32d8260dd3f6d72bb2a25278808
sang <lethanhsang14082001@gmail.com>
2023-06-05 01:16:20 +0700

    Add path type for enemy

--- Assets/Scripts/PathFollower.cs
+++ Assets/Scripts/PathFollower.cs
@@ -2,5 +2,11 @@ using UnityEngine;
 
 public class PathFollower : MonoBehaviour
 {
-    
+    /**
+     * Path Type :
+     *  1: Ground in map for solider
+     *  2: Air for plane (map2, map3)
+     */
+    public enum PathType { Ground, Air};
+
 }
