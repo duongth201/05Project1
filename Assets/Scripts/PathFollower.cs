47782c58c5c6a2f352f7e6b236b24f808ac09301
sang <lethanhsang14082001@gmail.com>
2023-06-05 01:19:58 +0700

    Add propertyes for Path Follower

--- Assets/Scripts/PathFollower.cs
+++ Assets/Scripts/PathFollower.cs
@@ -9,4 +9,19 @@ public class PathFollower : MonoBehaviour
      */
     public enum PathType { Ground, Air};
 
+
+    // Property for Path Follower
+    public PathType Type;
+    public float Speed; 
+    public float OffsetAmount;  
+
+    private Vector2 PositionOffset;
+    private Path path;
+    private int segmentIndex;   // index in map
+
+    private Vector2 A, B, C, D;
+    private Vector2 v1, v2, v3;
+    private float t;
+
+
 }
